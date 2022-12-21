608b3b6b8fe66688c96fef88968caaf00c86aca2
tuomasOamk <99179437+tuomasOamk@users.noreply.github.com>
2022-12-21 23:45:55 +0200

    Delete client.py

--- client.py
+++
@@ -1,8 +0,0 @@
-from cgi import print_directory
-import pandas as pd
-import csv
-
-connection = pd.read_csv("http://172.20.241.9/luedataa_kannasta_groupid_csv.php?groupid=68")
-connection.fillna(0, inplace=True)
-
-print(connection)
